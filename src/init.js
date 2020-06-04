54409a9ffd7c79f185177a6736094be33444e576
Kacper Kiermasz <6529076+orangeNotebook@users.noreply.github.com>
2020-06-04 12:34:54 +0100

    Reading instructions from file complete

--- src/init.js
+++ src/init.js
@@ -5,26 +5,41 @@ const gridCreate = require('../src/grid.js');
 
 module.exports = (gridX, gridY)=>{
     
-    var instructions = fs.readFileSync('../instructions/instructions.txt').toString().split("\n");
+    var instructions = fs.readFileSync('../instructions/instructions.txt').toString().replace('\r', "").split("\n");
     var gridInstructions = '';
     var startInstructions = '';
     var moveInstructions = '';
 
+    for(var a = 0; a <= instructions.length; a++){
+        var index = instructions.indexOf('\r');
+        if (index > -1) {
+            instructions.splice(index, 1);
+        }
+    }
+ 
     gridInstructions = instructions[0].replace('\r', "").split(" ");
-    startInstructions = instructions[1].replace('\r', "").split(" ");
-    moveInstructions = instructions[2].split("");
 
     // console.log(gridInstructions);
     // console.log(startInstructions);
     // console.log(moveInstructions);
 
     gridCreate(gridInstructions[0], gridInstructions[1], gridX, gridY);
-    rover = new roverClass(startInstructions[2],startInstructions[0],startInstructions[1]);
-    instructionTranslator(moveInstructions, rover);
 
     // console.log("After completing your instructions, your rover is facing " + rover.rotation + " and is at coordinates " + rover.xCoord + " " + rover.yCoord);
+   
+    for(var i = 1; i <= ((instructions.length - 1)); i = i + 2){
+        
+        startInstructions = instructions[i].replace('\r', "").split(" ");
+        moveInstructions = instructions[i + 1].replace('\r', "").split("");
 
-    console.log(rover.xCoord + " " + rover.yCoord + " " + rover.rotation);
+        rover = new roverClass(startInstructions[2],startInstructions[0],startInstructions[1]);
+        instructionTranslator(moveInstructions, rover);
+
+        console.log(rover.xCoord + " " + rover.yCoord + " " + rover.rotation);
+        
+    }
+    
+    
 
     
 }
